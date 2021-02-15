3dc31973279cc701d8309baa95dfbc13cfecd9bc
Andrey Bogoyavlensky <abogoyavlensky@gmail.com>
2021-02-16 00:08:48 +0300

    Add initital simplest ability for migration

--- src/tuna/migrations.clj
+++ src/tuna/migrations.clj
@@ -1,8 +1,310 @@
 (ns tuna.migrations
-  (:require [clojure.spec.alpha :as s]))
+  (:require [clojure.java.jdbc :as jdbc]
+            [clojure.spec.alpha :as s]
+            [clojure.edn :as edn]
+            [clojure.java.io :as io]
+            [clojure.string :as str]
+            [clojure.pprint :as pprint]
+            [honeysql.core :as hsql]
+            [honeysql-postgres.format :as hformat]
+            [honeysql-postgres.helpers :as phsql]))
+
+; Config
+
+(def ^:private MODELS-PATH
+  "Path to models' definitions file."
+  "src/rhino/models.edn")
 
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (prn x "Hello, World!"))
+(def ^:private MIGRATION-DIR
+  "Root dir for migrations."
+  "src/rhino/migrations")
+
+
+(def ^:private MIGRATIONS-TABLE
+  "Default migrations table name."
+  :migrations)
+
+
+(defn- models
+  "Return models' definitions."
+  []
+  (-> (slurp MODELS-PATH)
+    (edn/read-string)))
+
+
+(defn- read-migration
+  "Return models' definitions."
+  [file-name]
+  (-> (slurp (str MIGRATION-DIR "/" file-name))
+    (edn/read-string)))
+
+
+(defn- create-migrations-dir
+  "Create migrations root dir if it is not exist."
+  []
+  (when-not (.isDirectory (io/file MIGRATION-DIR))
+    (.mkdir (java.io.File. MIGRATION-DIR))))
+
+
+(defn- db-conn
+  "Return db connection for performing migration."
+  []
+  (let [db-uri (or (System/getenv "RHINO_DB_URL")
+                 "jdbc:postgresql://localhost:5432/rhino?user=rhino&password=rhino")]
+    {:connection-uri db-uri}))
+
+
+(defn- table-exists?
+  "Check if table exists."
+  [table-name]
+  (->> {:select [:*]
+        :from [:information_schema.tables]
+        :where [:and
+                [:= :table_name table-name]
+                [:= :table_schema "public"]]}
+    (hsql/format)
+    (jdbc/query (db-conn))
+    (seq)
+    (some?)))
+
+
+(defn- create-migrations-table
+  "Create table to keep migrations history."
+  []
+  (when-not (table-exists? (name MIGRATIONS-TABLE))
+    (->> {:create-table [MIGRATIONS-TABLE]
+          :with-columns [[[:id :serial (hsql/call :not nil) (hsql/call :primary-key)]
+                          [:name (hsql/call :varchar (hsql/inline 256)) (hsql/call :not nil) (hsql/call :unique)]
+                          [:created_at :timestamp (hsql/call :default (hsql/call :now))]]]}
+      (hsql/format)
+      (jdbc/execute! (db-conn)))))
+
+
+(defn- save-migration
+  "Save migration to db after applying it."
+  [migration-name]
+  (->> {:insert-into MIGRATIONS-TABLE
+        :values [{:name migration-name}]}
+    (hsql/format)
+    (jdbc/execute! (db-conn))))
+
+
+; Spec
+
+(s/def :field/type #{:int
+                     :serial
+                     :varchar
+                     :text
+                     :timestamp})
+
+
+(s/def :field/null boolean?)
+(s/def :field/primary boolean?)
+(s/def :field/max-length pos-int?)
+
+
+(s/def ::field
+  (s/keys
+    :req-un [:field/type]
+    :opt-un [:field/null
+             :field/primary
+             :field/max-length]))
+
+
+(s/def :model/fields
+  (s/map-of keyword? ::field))
+
+
+(s/def ::model
+  (s/keys
+    :req-un [:model/fields]))
+
+
+(s/def ::models
+  (s/map-of keyword? ::model))
+
+
+(s/def ::model->action
+  (s/conformer
+    (fn [value]
+      (assoc value :action :create-table))))
+
+
+(s/def ::->action
+  (s/and
+    (s/cat
+      :name keyword?
+      :model ::model)
+    ::model->action))
+
+
+(s/def ::->migration
+  (s/and
+    ::->action))
+
+
+(defn- migrations-list
+  "Get migrations' files list."
+  []
+  (->> (file-seq (io/file MIGRATION-DIR))
+    (filter #(.isFile %))
+    (map #(.getName %))))
+
+
+(defn- next-migration-number
+  [file-names]
+  (if (empty? file-names)
+    "0000"
+    ; TODO: define next migration number
+    "0000"))
+
+
+(defn- next-migration-name
+  [actions]
+  (let [action-name (-> actions first :action name)
+        model-name (-> actions first :name name)]
+    (-> (str action-name "_" model-name)
+      (str/replace #"-" "_"))))
+
+
+(defn make-migrations
+  "Make new migrations based on models' definitions automatically."
+  [_]
+  (create-migrations-dir)
+  (let [new-model (first (models))
+        migration (s/conform ::->migration new-model)
+        migration-names (migrations-list)
+        migration-number (next-migration-number migration-names)
+        migration-name (next-migration-name [migration])
+        migration-file-name (str migration-number "_" migration-name)]
+    (spit (str MIGRATION-DIR "/" migration-file-name ".edn")
+      (with-out-str
+        (pprint/pprint [migration])))))
+
+
+(s/def :option->sql/type
+  (s/conformer
+    (fn [value]
+      ;(hsql/call value)
+      (hsql/raw (name value)))))
+
+
+(s/def :option->sql/null
+  (s/conformer
+    (fn [value]
+      (if (true? value)
+        (hsql/call nil)
+        (hsql/call :not nil)))))
+
+
+(s/def ::options->sql
+  (s/keys
+    :req-un [:option->sql/type]
+    :opt-un [:option->sql/null]))
+
+
+(s/def :action/action #{:create-table})
+(s/def :action/name keyword?)
+
+
+(s/def :model->/fields
+  (s/map-of keyword? ::options->sql))
+
+
+(s/def :action/model
+  (s/keys
+    :req-un [:model->/fields]))
+
+
+(defn- fields->columns
+  [fields]
+  (->> fields
+    (reduce (fn [acc [k v]]
+              (conj acc (->> (vals v)
+                          (cons k)
+                          (vec)))) [])))
+
+
+(s/def ::create-model->sql
+  (s/conformer
+    (fn [value]
+      {(:action value) [(:name value)]
+       :with-columns [(fields->columns (-> value :model :fields))]})))
+
+
+(s/def ::->sql
+  (s/conformer
+    #(hsql/format %)))
+
+
+(s/def ::action->sql
+  (s/and
+    (s/keys
+      :req-un [:action/action
+               :action/name
+               :action/model])
+    ::create-model->sql
+    ::->sql))
+
+;(-> {:create-table [(:name action)]
+;     :with-columns [[[:id (hsql/raw "serial") (hsql/call :not nil)]]]}
+;    (hsql/format)))))
+
+
+(defn- sql
+  "Generate raw sql from migration."
+  [_]
+  (let [migration-names (migrations-list)
+        file-name (first migration-names)
+        actions (read-migration file-name)
+        action (first actions)]
+    (s/conform ::action->sql action)))
+
+
+(defn migrate
+  "Run migration on a db."
+  [_]
+  (let [migration-names (migrations-list)
+        file-name (first migration-names)
+        migration-name (first (str/split file-name #"\."))
+        actions (read-migration file-name)
+        action (first actions)
+        migration-sql (s/conform ::action->sql action)]
+    (create-migrations-table)
+    (jdbc/with-db-transaction [tx (db-conn)]
+      (jdbc/execute! tx migration-sql)
+      (save-migration migration-name))))
+
+
+(comment
+  (let [x 1]
+    ;(s/explain ::models (models))
+    ;(s/valid? ::models (models))
+    ;(s/conform ::->migration (first (models)))))
+    ;(make-migrations {})))
+    (migrate {})))
+
+;(table-exists? "migrations")))
+
+;(->> (get-in action [:model :fields])
+;  (reduce (fn [acc [k v]]
+;            (conj acc (->> (vals v)
+;                        (cons k)
+;                        (vec)))) []))))
+
+
+;(conj acc [k (vals v)])) [])))
+
+;(-> {:create-table [:feed]
+;     :with-columns [[[:id (hsql/call :serial) (hsql/call :not nil)]]]}
+;    (hsql/format))))
+
+;(-> (phsql/create-table :films)
+;    (phsql/with-columns [[:code (hsql/call :char 5) (hsql/call :constraint :firstkey) (hsql/call :primary-key)]
+;                         [:title (hsql/call :varchar 40) (hsql/call :not nil)]
+;                         [:did :integer (hsql/call :not nil)]
+;                         [:date_prod :date]
+;                         [:kind (hsql/call :varchar 10)]])
+;    hsql/format)))
+;    (hsql/raw :serial)))
