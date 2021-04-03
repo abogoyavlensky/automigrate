4d92d3709e2f0667477ee517776f87b94452deb1
Andrey Bogoyavlensky <abogoyavlensky@gmail.com>
2021-04-03 14:02:29 +0300

    Getting state of simple creating model

--- src/tuna/schema.clj
+++ src/tuna/schema.clj
@@ -8,28 +8,20 @@
   (map util-file/read-file-obj migrations-files))
 
 
-(defmulti apply-migration
-  "Apply migration to schema."
+(defmulti apply-migration-to-schema
   (fn [_schema migration]
     (:action migration)))
 
 
-(defmethod apply-migration sql/CREATE-TABLE-ACTION
+(defmethod apply-migration-to-schema sql/CREATE-TABLE-ACTION
   [schema migration]
   (assoc schema (:name migration) (:model migration)))
 
 
-(defn- make-schema
-  ([]
-   (make-schema [] nil))
-  ([schema migration]
-   (apply-migration schema migration)))
-
-
 (defn current-db-schema
-  "Returns map of models derived from existing migrations."
+  "Return map of models derived from existing migrations."
   [migrations-files]
-  ; TODO: add validation of migrations!
-  (let [migrations (load-migrations-from-files migrations-files)]
-    ; TODO: reduce all migrations not only first!
-    (reduce make-schema (first migrations))))
+  ; TODO: add validation of migrations with spec!
+  (let [migrations (-> (load-migrations-from-files migrations-files)
+                     (flatten))]
+    (reduce apply-migration-to-schema {} migrations)))
