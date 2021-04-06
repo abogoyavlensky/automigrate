ff9671f867829bfdc34f397616c8e8127002d96f
Andrey Bogoyavlensky <abogoyavlensky@gmail.com>
2021-04-06 22:58:04 +0300

    Add test for creating migrations dir

--- test/tuna/migrations_test.clj
+++ test/tuna/migrations_test.clj
@@ -1,5 +1,6 @@
 (ns tuna.migrations-test
   (:require [clojure.test :refer :all]
+            [clojure.java.io :as io]
             [tuna.migrations :as migrations])
   (:import [java.io FileNotFoundException]))
 
@@ -9,10 +10,18 @@
     (is (= {:feed
             {:fields {:id {:type :serial
                            :null false}}}}
-           (#'migrations/models path)))))
+          (#'migrations/models path)))))
 
 
 (deftest test-reading-models-from-file-err
   (let [path "test/tuna/models/not_existing.edn"]
     (is (thrown? FileNotFoundException
-           (#'migrations/models path)))))
+          (#'migrations/models path)))))
+
+
+(deftest test-create-migrations-dir-ok
+  (let [path "test/tuna/migrations"]
+    (is (false? (.isDirectory (io/file path))))
+    (#'migrations/create-migrations-dir path)
+    (is (true? (.isDirectory (io/file path))))
+    (io/delete-file path)))
