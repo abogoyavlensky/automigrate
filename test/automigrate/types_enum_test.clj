2b87e6a6c8c19dec5fe15a21fefab8107ddc45be
Andrey Bogoyavlenskiy <abogoyavlensky@gmail.com>
2023-10-10 23:40:22 +0100

    Add test for creating table without enum and type at once

--- test/automigrate/types_enum_test.clj
+++ test/automigrate/types_enum_test.clj
@@ -141,3 +141,56 @@
                       (constantly existing-actions)]
                      [file-util/read-edn (constantly existing-models)]]
       (is (= [] (#'migrations/make-migration* "" []))))))
+
+
+(deftest test-make-and-migrate-create-type-enum-with-creating-table-ok
+  (let [existing-actions '()
+        changed-models {:account
+                        {:fields [[:id :serial]]
+                         :types [[:account-role :enum {:choices ["admin" "customer"]}]]}}
+        expected-actions '({:action :create-table
+                            :model-name :account
+                            :fields {:id {:type :serial}}}
+                           {:action :create-type
+                            :model-name :account
+                            :type-name :account-role
+                            :options {:type :enum
+                                      :choices ["admin" "customer"]}})
+        expected-q-edn '({:create-table [:account]
+                          :with-columns [(:id :serial)]}
+                         {:create-type
+                          [:account-role :as (:enum "admin" "customer")]})
+        expected-q-sql (list
+                         ["CREATE TABLE account (id SERIAL)"]
+                         ["CREATE TYPE account_role AS ENUM('admin', 'customer')"])]
+
+    (test-util/test-make-and-migrate-ok!
+      existing-actions
+      changed-models
+      expected-actions
+      expected-q-edn
+      expected-q-sql)
+
+    (testing "check created type in db"
+      (is (= [{:typname "account_role"
+               :typtype "e"
+               :enumlabel "admin"}
+              {:typname "account_role"
+               :typtype "e"
+               :enumlabel "customer"}]
+            (db-util/exec!
+              config/DATABASE-CONN
+              {:select [:t.typname :t.typtype :e.enumlabel]
+               :from [[:pg_type :t]]
+               :join [[:pg_enum :e] [:= :e.enumtypid :t.oid]]
+               :where [:= :t.typname "account_role"]}))))
+
+    (testing "test actual db schema after applying the migration"
+      (is (= [{:character_maximum_length nil
+               :column_default "nextval('account_id_seq'::regclass)"
+               :column_name "id"
+               :data_type "integer"
+               :udt_name "int4"
+               :is_nullable "NO"
+               :table_name "account"}]
+            (test-util/get-table-schema-from-db config/DATABASE-CONN "account"))))))
