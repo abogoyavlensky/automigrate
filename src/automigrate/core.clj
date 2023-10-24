e7d704b4ea05817db822069d80ca60e9578b88e2
Andrey Bogoyavlenskiy <abogoyavlensky@gmail.com>
2023-10-24 23:56:42 +0100

    Add help command

--- src/automigrate/core.clj
+++ src/automigrate/core.clj
@@ -14,6 +14,9 @@
 (s/def ::migrations-dir string?)
 (s/def ::jdbc-url (s/conformer str))
 (s/def ::number int?)
+(def HELP-CMDS-ORDER
+  ['make 'migrate 'list 'explain 'help])
+(s/def ::cmd (set HELP-CMDS-ORDER))
 
 
 (s/def ::format
@@ -76,6 +79,11 @@
     :opt-un [::migrations-table]))
 
 
+(s/def ::help-args
+  (s/keys
+    :opt-un [::cmd]))
+
+
 (defn- run-fn
   [f args args-spec]
   (try+
@@ -96,12 +104,12 @@
 (defn make
   "Create new migration based on changes of models.
 
-  Available options:
-    - :models-file - path to models' file (required)
-    - :migrations-dir - path to dir for storing migrations (required)
-    - :name - custom name for migration (optional)
-    - :type - type of new migration, empty by default for auto-migration;
-              also available :empty-sql - for creating empty raw SQL migration; (optional)"
+Available options:
+  :models-file - path to models' file (required)
+  :migrations-dir - path to dir for storing migrations (required)
+  :name - custom name for migration (optional)
+  :type - type of new migration, empty by default for auto-migration;
+          also available :empty-sql - for creating empty raw SQL migration; (optional)"
   [args]
   (run-fn migrations/make-migration args ::make-args))
 
@@ -109,23 +117,23 @@
 (defn migrate
   "Run existing migrations and change database schema.
 
-  Available options:
-  - :jdbc-url - jdbc url of database connection (required)
-  - :migrations-dir - path to dir for storing migrations (required)
-  - :migrations-table - optional migrations' table name (optional)
-  - :number - integer number of target point migration (optional)"
+Available options:
+  :jdbc-url - jdbc url of database connection (required)
+  :migrations-dir - path to dir for storing migrations (required)
+  :migrations-table - optional migrations' table name (optional)
+  :number - integer number of target point migration (optional)"
   [args]
   (run-fn migrations/migrate args ::migrate-args))
 
 
 (defn explain
-  "Show raw SQL for migration by number
+  "Show raw SQL for migration by number.
 
-  Available options:
-  - :migrations-dir - path to dir for storing migrations (required)
-  - :number - integer number of migration to explain (required)
-  - :direction - direction for SQL from migration (optional)
-  - :format - format of explanation, can be `sql` or `human`, `sql` by default (optional)"
+Available options:
+  :migrations-dir - path to dir for storing migrations (required)
+  :number - integer number of migration to explain (required)
+  :direction - direction for SQL from migration (optional)
+  :format - format of explanation, can be `sql` or `human`, `sql` by default (optional)"
   [args]
   (run-fn migrations/explain args ::explain-args))
 
@@ -133,9 +141,53 @@
 (defn list
   "Show list of existing migrations with status.
 
-  Available options:
-  - :jdbc-url - jdbc url of database connection (required)
-  - :migrations-dir - path to dir for storing migrations (required)
-  - :migrations-table - optional migrations' table name (optional)"
+Available options:
+  :jdbc-url - jdbc url of database connection (required)
+  :migrations-dir - path to dir for storing migrations (required)
+  :migrations-table - optional migrations' table name (optional)"
   [args]
   (run-fn migrations/list-migrations args ::list-args))
+
+(defn- fn-docstring
+  [public-methods fn-sym]
+  (-> public-methods (get fn-sym) (meta) :doc (str "\n")))
+
+(defn- general-help
+  [public-methods]
+  (let [all-cmd-descs (reduce
+                        (fn [acc cmd]
+                          (let [cmd-desc (->> (fn-docstring public-methods cmd)
+                                              (str/split-lines)
+                                              (first))]
+                            (conj acc (str "  " cmd " - " cmd-desc))))
+                        []
+                        HELP-CMDS-ORDER)]
+    (str/join
+      "\n"
+      (concat
+        ["Database auto-migration tool for Clojure.\n"
+         "Available commands:"]
+        all-cmd-descs))))
+
+
+(defn- print-out-str
+  [doc-str]
+  (.write *out*
+    doc-str))
+
+
+(defn- help*
+  [{:keys [cmd]}]
+  (let [public-methods (ns-publics 'automigrate.core)]
+    (if (some? cmd)
+      (-> public-methods (fn-docstring cmd) (print-out-str))
+      (-> public-methods (general-help) (print-out-str)))))
+
+
+(defn help
+  "Help information for all commands of automigrate.
+
+Available options:
+  :cmd - command name; show help for particular command. (optional)"
+  [args]
+  (run-fn help* args ::help-args))
