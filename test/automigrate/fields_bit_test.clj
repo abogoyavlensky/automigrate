8b73dc3fc8112b32962c2ad31fda15d7a1f8502d
Andrey Bogoyavlenskiy <abogoyavlensky@gmail.com>
2023-11-04 23:49:27 +0000

    Add col tests

--- test/automigrate/fields_bit_test.clj
+++ test/automigrate/fields_bit_test.clj
@@ -58,24 +58,86 @@
                                   :model-name :account
                                   :options {:type [field-type 10]}})
               :q-edn [{:create-table [:account]
-                       :with-columns [(list :thing [field-type 3])]}
+                       :with-columns ['(:id :serial)]}
+                      {:add-column (list :thing [field-type 3])
+                       :alter-table :account}
                       {:alter-table (list :account
                                       {:alter-column [:thing :type [field-type 10]]})}]
-              :q-sql [[(format "CREATE TABLE account (thing %s(3))"
+              :q-sql [["CREATE TABLE account (id SERIAL)"]
+                      [(format "ALTER TABLE account ADD COLUMN thing %s(3)"
                          (str/upper-case (name field-type)))]
                       [(format "ALTER TABLE account ALTER COLUMN thing TYPE %s(10)"
                          (str/upper-case (name field-type)))]]}
             (test-util/perform-make-and-migrate!
               {:jdbc-url config/DATABASE-CONN
                :existing-actions [{:action :create-table
-                                   :fields {:thing {:type [field-type 3]}}
-                                   :model-name :account}]
+                                   :fields {:id {:type :serial}}
+                                   :model-name :account}
+                                  {:action :add-column
+                                   :field-name :thing
+                                   :model-name :account
+                                   :options {:type [field-type 3]}}]
                :existing-models {:account
-                                 {:fields [[:thing [field-type 10]]]}}})))
+                                 {:fields [[:id :serial]
+                                           [:thing [field-type 10]]]}}})))
 
       (testing "check actual db changes"
         (testing "test actual db schema after applying the migration"
-          (is (= [{:character_maximum_length 10
+          (is (= [{:character_maximum_length nil
+                   :column_default "nextval('account_id_seq'::regclass)"
+                   :column_name "id"
+                   :data_type "integer"
+                   :udt_name "int4"
+                   :is_nullable "NO"
+                   :table_name "account"}
+                  {:character_maximum_length 10
+                   :column_default nil
+                   :column_name "thing"
+                   :data_type (or field-name (name field-type))
+                   :udt_name (name field-type)
+                   :is_nullable "YES"
+                   :table_name "account"}]
+                (test-util/get-table-schema-from-db config/DATABASE-CONN "account"))))))))
+
+
+(deftest ^:eftest/slow test-fields-bit-add-column-ok
+  (doseq [{:keys [field-type field-name]} [{:field-type :bit}
+                                           {:field-type :varbit
+                                            :field-name "bit varying"}]]
+    (test-util/drop-all-tables config/DATABASE-CONN)
+    (test-util/delete-recursively config/MIGRATIONS-DIR)
+
+    (testing "check generated actions, queries edn and sql from all actions"
+      (is (= {:new-actions (list {:action :add-column
+                                  :field-name :thing
+                                  :model-name :account
+                                  :options {:type [field-type 3]}})
+              :q-edn [{:create-table [:account]
+                       :with-columns ['(:id :serial)]}
+                      {:add-column (list :thing [field-type 3])
+                       :alter-table :account}]
+              :q-sql [["CREATE TABLE account (id SERIAL)"]
+                      [(format "ALTER TABLE account ADD COLUMN thing %s(3)"
+                         (str/upper-case (name field-type)))]]}
+            (test-util/perform-make-and-migrate!
+              {:jdbc-url config/DATABASE-CONN
+               :existing-actions [{:action :create-table
+                                   :fields {:id {:type :serial}}
+                                   :model-name :account}]
+               :existing-models {:account
+                                 {:fields [[:id :serial]
+                                           [:thing [field-type 3]]]}}})))
+
+      (testing "check actual db changes"
+        (testing "test actual db schema after applying the migration"
+          (is (= [{:character_maximum_length nil
+                   :column_default "nextval('account_id_seq'::regclass)"
+                   :column_name "id"
+                   :data_type "integer"
+                   :udt_name "int4"
+                   :is_nullable "NO"
+                   :table_name "account"}
+                  {:character_maximum_length 3
                    :column_default nil
                    :column_name "thing"
                    :data_type (or field-name (name field-type))
